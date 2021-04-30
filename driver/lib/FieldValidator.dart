7f520a2be36f59f1cf012e76e48f469497ffe400
khanh21011999 <37074707+khanh21011999@users.noreply.github.com>
2021-04-30 07:33:17 +0700

    update test

---
+++ driver/lib/FieldValidator.dart
@@ -0,0 +1,32 @@
+class  FieldValidator {
+  validateEmail(){
+
+  }
+  validateShortName(){
+
+  }
+  validateLongName(){
+
+  }
+  validateShortPass(){
+
+  }
+  validateLongPass(){
+
+  }
+  validateValidPass(){
+
+  }
+  validateValidName(){
+
+  }
+  validateLongPhoneNumber(){
+
+  }
+  validateShortPhoneNumber(){
+
+  }
+  validateClick(){
+
+  }
+}
--- driver/test/widget_test.dart
+++ driver/test/widget_test.dart
@@ -1,66 +1,64 @@
-// // This is a basic Flutter widget test.
-// //
-// // To perform an interaction with a widget in your test, use the WidgetTester
-// // utility that Flutter provides. For example, you can send tap and scroll
-// // gestures. You can also use WidgetTester to find child widgets in the widget
-// // tree, read text, and verify that the values of widget properties are correct.
+// This is a basic Flutter widget test.
 //
-// import 'package:flutter/material.dart';
-// import 'package:flutter_test/flutter_test.dart';
-//
-// import 'package:driver/main.dart';
-//
-// void main() {
-//   testWidgets('Counter increments smoke test', (WidgetTester tester) async {
-//     // Build our app and trigger a frame.
-//     await tester.pumpWidget(MyApp());
-//
-//     // Verify that our counter starts at 0.
-//     expect(find.text('0'), findsOneWidget);
-//     expect(find.text('1'), findsNothing);
-//
-//     // Tap the '+' icon and trigger a frame.
-//     await tester.tap(find.byIcon(Icons.add));
-//     await tester.pump();
-//
-//     // Verify that our counter has incremented.
-//     expect(find.text('0'), findsNothing);
-//     expect(find.text('1'), findsOneWidget);
-//     test('Empty Email Test', () {
-//       var result = FieldValidator.validateEmail('');
-//       expect(result, 'Enter Email!');
-//     });
-//
-//
-//     test('Invalid Email Test', () {
-//       var result = FieldValidator.validateEmail('');
-//       expect(result, 'Enter Valid Email!');
-//     });
-//     test('Empty Email Test', () {
-//       var result = LoginPage().loginUser(BuildContext context);
-//       expect(result, 'You are loggin now');
-//     });
-//     test('Empty Email Test', () {
-//       var result = FieldValidator.validateEmail('');
-//       expect(result, 'Enter Email!');
-//     });
-//
-//     test('Empty Password Test', () {
-//       var result = FieldValidator.validatePassword('');
-//       expect(result, 'Enter Password!');
-//     });
-//
-//     test('Valid Password Test', () {
-//       var result = FieldValidator.validatePassword('ajay12345');
-//       expect(result, null);
-//     });
-//     test('Empty Email Test', () {
-//       var result = registerUser(BuildContext context){
-//         expect(result, 'Error');
-//       });
-//   }
-//   );
-// }
-//
-// class FieldValidator {
-// }
+// To perform an interaction with a widget in your test, use the WidgetTester
+// utility that Flutter provides. For example, you can send tap and scroll
+// gestures. You can also use WidgetTester to find child widgets in the widget
+// tree, read text, and verify that the values of widget properties are correct.
+
+import 'package:flutter/material.dart';
+import 'package:flutter_test/flutter_test.dart';
+
+import 'package:driver/main.dart';
+import 'package:driver/FieldValidator.dart';
+
+void main() {
+  testWidgets('Counter increments smoke test', (WidgetTester tester) async {
+    // Build our app and trigger a frame.
+    await tester.pumpWidget(MyApp());
+
+    // Verify that our counter starts at 0.
+    expect(find.text('0'), findsOneWidget);
+    expect(find.text('1'), findsNothing);
+
+    // Tap the '+' icon and trigger a frame.
+    await tester.tap(find.byIcon(Icons.add));
+    await tester.pump();
+
+    // Verify that our counter has incremented.
+    expect(find.text('0'), findsNothing);
+    expect(find.text('1'), findsOneWidget);
+    test('Valid Email', () {
+      var result = FieldValidator().validateEmail();
+      expect(result, 'Invalid Email');
+    });
+
+
+    test('Valid Button', () {
+      var result = FieldValidator().validateClick();
+      expect(result, 'Invalid Button');
+    });
+    test('Long Name', () {
+      var result = FieldValidator().validateLongName();
+      expect(result, 'Long Name');
+    });
+    test('Long Pass', () {
+      var result = FieldValidator().validateLongPass();
+      expect(result, 'Long Pass');
+    });
+
+    test('Long Phone Number', () {
+      var result = FieldValidator()..validateLongPhoneNumber();
+      expect(result, 'Long Phone Number');
+    });
+
+    test('Valid Short Phone Number', () {
+      var result = FieldValidator().validateShortPhoneNumber();
+      expect(result, 'Short Phone Number');
+    });
+    test('Validate Short Pass', () {
+      var result = FieldValidator().validateShortPass();
+      expect(result, 'Short Pass');
+      });
+    }
+    );
+  }
