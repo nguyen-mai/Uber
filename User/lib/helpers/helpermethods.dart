dee9a11136d2694d0a5ccb66562346c717fed42a
khanh21011999 <37074707+khanh21011999@users.noreply.github.com>
2021-05-13 10:56:18 +0700

    Fix

--- User/lib/helpers/helpermethods.dart
+++ User/lib/helpers/helpermethods.dart
@@ -1,118 +1,116 @@
-
 import 'dart:convert';
 import 'dart:math';
 
-import 'package:uber/datamodels/address.dart';
-import 'package:uber/datamodels/directiondetails.dart';
-import 'package:uber/datamodels/user.dart';
-import 'package:uber/dataprovider/appdata.dart';
-import 'package:uber/globalvariable.dart';
-import 'package:uber/helpers/requesthelper.dart';
 import 'package:connectivity/connectivity.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:firebase_database/firebase_database.dart';
 import 'package:geolocator/geolocator.dart';
 import 'package:google_maps_flutter/google_maps_flutter.dart';
-import 'package:http/http.dart' as http;
+import 'package:uber/DataProvider/appdata.dart';
+import 'package:uber/Helper/RequestHelper.dart';
+import 'package:uber/datamodels/address.dart';
+import 'package:uber/datamodels/directiondetails.dart';
+import 'package:uber/datamodels/user.dart';
+import 'package:uber/globevariable.dart';
 import 'package:provider/provider.dart';
+import 'package:http/http.dart' as http;
 
+class HelperMethod {
+  static void getCurrentUserInfo() async {
+    currentFirebaseUser = await FirebaseAuth.instance.currentUser;
+    String userId = currentFirebaseUser.uid;
 
-class HelperMethods{
+    DatabaseReference usersRef =
+    FirebaseDatabase.instance.reference().child('users').child(userId);
+    usersRef.once().then((DataSnapshot snapshot) {
+      if (snapshot.value != null) {
+        //print(snapshot.value);
+        currentUserInfo = UserInformation.fromSnapshot(snapshot);
+        //print('My name is ${currentUserInfo.fullName}');
+        userName = currentUserInfo.fullName;
+        //print(userName);
+        //print(userId);
 
-  static void getCurrentUserInfo() async{
-
-    currentFirebaseUser = await FirebaseAuth.instance.currentUser();
-    String userid = currentFirebaseUser.uid;
-
-    DatabaseReference userRef = FirebaseDatabase.instance.reference().child('users/$userid');
-    userRef.once().then((DataSnapshot snapshot){
-
-      if(snapshot.value != null){
-        currentUserInfo = User.fromSnapshot(snapshot);
-        print('my name is ${currentUserInfo.fullName}');
       }
-
     });
   }
 
- static Future<String> findCordinateAddress(Position position, context) async {
+  static Future<String> findCordinateAndress(Position position, context) async {
+    String placeAddress = '';
+    var connectivityReslut = await Connectivity().checkConnectivity();
+    if (connectivityReslut != ConnectivityResult.mobile &&
+        connectivityReslut != ConnectivityResult.wifi) {
+      return placeAddress;
+    }
+    var url = Uri.parse(
+        'https://maps.googleapis.com/maps/api/geocode/json?latlng=${position.latitude},${position.longitude}&key=${mapKey}');
+    var response = await RequestHelper.getRequest(url);
+    if (response != "failed") {
+      placeAddress = response['results'][0]['formatted_address'];
+      Address pickupAddress = new Address();
+      pickupAddress.longitude = position.longitude;
+      pickupAddress.latitude = position.latitude;
+      pickupAddress.placeName = placeAddress;
 
-   String placeAddress = '';
-
-   var connectivityResult = await Connectivity().checkConnectivity();
-   if(connectivityResult != ConnectivityResult.mobile && connectivityResult != ConnectivityResult.wifi){
-     return placeAddress;
-   }
-
-   String url = 'https://maps.googleapis.com/maps/api/geocode/json?latlng=${position.latitude},${position.longitude}&key=$mapKey';
-
-   var response = await RequestHelper.getRequest(url);
-
-   if(response != 'failed'){
-     placeAddress = response['results'][0]['formatted_address'];
-
-     Address pickupAddress = new Address();
-
-     pickupAddress.longitude = position.longitude;
-     pickupAddress.latitude = position.latitude;
-     pickupAddress.placeName = placeAddress;
-
-     Provider.of<AppData>(context, listen: false).updatePickupAddress(pickupAddress);
-
-   }
-
-   return placeAddress;
-
+      Provider.of<AppData>(context, listen: false)
+          .updatePickupAddress(pickupAddress);
+    }
+    /* if(response=="failed"){
+      return "Unknown";
+    }*/
+    return placeAddress;
   }
 
-  static Future<DirectionDetails> getDirectionDetails(LatLng startPosition, LatLng endPosition) async {
+  static Future<DirectionDetails> getDirectionDetails(
+      LatLng startPosition, LatLng endPosition) async {
+    var url = Uri.parse(
+        'https://maps.googleapis.com/maps/api/directions/json?origin=${startPosition.latitude},${startPosition.longitude}&destination=${endPosition.latitude},${endPosition.longitude}&mode=driving&key=$mapKey');
 
-   String url = 'https://maps.googleapis.com/maps/api/directions/json?origin=${startPosition.latitude},${startPosition.longitude}&destination=${endPosition.latitude},${endPosition.longitude}&mode=driving&key=$mapKey';
+    var response = await RequestHelper.getRequest(url);
 
-   var response = await RequestHelper.getRequest(url);
+    if (response == 'failed') {
+      return null;
+    }
 
-   if(response == 'failed'){
-     return null;
-   }
+    DirectionDetails directionDetails = DirectionDetails();
 
-   DirectionDetails directionDetails = DirectionDetails();
+    directionDetails.durationText =
+    response['routes'][0]['legs'][0]['duration']['text'];
+    directionDetails.durationValue =
+    response['routes'][0]['legs'][0]['duration']['value'];
 
-   directionDetails.durationText = response['routes'][0]['legs'][0]['duration']['text'];
-   directionDetails.durationValue = response['routes'][0]['legs'][0]['duration']['value'];
+    directionDetails.distanceText =
+    response['routes'][0]['legs'][0]['distance']['text'];
+    directionDetails.distanceValue =
+    response['routes'][0]['legs'][0]['distance']['value'];
 
-   directionDetails.distanceText = response['routes'][0]['legs'][0]['distance']['text'];
-   directionDetails.distanceValue = response['routes'][0]['legs'][0]['distance']['value'];
+    directionDetails.encodedPoints =
+    response['routes'][0]['overview_polyline']['points'];
 
-   directionDetails.encodedPoints = response['routes'][0]['overview_polyline']['points'];
-
-   return directionDetails;
+    return directionDetails;
   }
 
-  static int estimateFares (DirectionDetails details){
-   // per km = $0.3,
-    // per minute = $0.2,
-    // base fare = $3,
-
-    double baseFare = 3;
-    double distanceFare = (details.distanceValue/1000) * 0.3;
-    double timeFare = (details.durationValue / 60) * 0.2;
-
-    double totalFare = baseFare + distanceFare + timeFare;
-
+  static int estimateFares(DirectionDetails details) {
+    //1km=6.900d
+    //base fare=10.000d
+    //time fare= 2.000d/min
+    double basefare = 10000;
+    double distanceFare = (details.distanceValue / 1000) * 6900;
+    double timeFare = (details.durationValue / 60) * 2000;
+    double totalFare = basefare + distanceFare + timeFare;
     return totalFare.truncate();
   }
 
-  static double generateRandomNumber(int max){
-
+  static double generateRandomNumber(int max) {
     var randomGenerator = Random();
     int randInt = randomGenerator.nextInt(max);
 
     return randInt.toDouble();
   }
 
-  static sendNotification(String token, context, String ride_id) async {
-
+  static sendNotification(String token, context, String userID) async {
     var destination = Provider.of<AppData>(context, listen: false).destinationAddress;
+
 
     Map<String, String> headerMap = {
       'Content-Type': 'application/json',
@@ -121,31 +119,29 @@ class HelperMethods{
 
     Map notificationMap = {
       'title': 'NEW TRIP REQUEST',
-      'body': 'Destination, ${destination.placeName}'
+      'body': 'Destination, ${destination.placeName}',
     };
 
     Map dataMap = {
       'click_action': 'FLUTTER_NOTIFICATION_CLICK',
       'id': '1',
       'status': 'done',
-      'ride_id' : ride_id,
+      'user_id': userID,
     };
 
     Map bodyMap = {
       'notification': notificationMap,
       'data': dataMap,
       'priority': 'high',
-      'to': token
+      'to': token,
     };
 
     var response = await http.post(
-      'https://fcm.googleapis.com/fcm/send',
-      headers: headerMap,
-      body: jsonEncode(bodyMap)
+        'https://fcm.googleapis.com/fcm/send',
+        headers: headerMap,
+        body: jsonEncode(bodyMap)
     );
 
     print(response.body);
-
   }
-
 }
